16e6998b5ee82e61429013bd1d3ca315c4539ea5
Alexander Blunck <alex@blunck.co>
2018-05-15 19:21:16 +0200

    Use webpack for build step

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = function (env, argv) {
+    let devtool
+
+    if (argv.mode === 'development') {
+        devtool = 'sourcemap'
+    }
+
+    return {
+        output: {
+            path: path.resolve(__dirname, 'lib'),
+            filename: `react-i18n.js`,
+            library: 'reactI18n',
+            libraryTarget: 'umd'
+        },
+        devtool: devtool,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                }
+            ]
+        },
+        externals: [
+            'object-path',
+            'prop-types',
+            'react'
+        ]
+    }
+}
